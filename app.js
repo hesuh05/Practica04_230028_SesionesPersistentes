cc5b0bc71e5845bf25827b90d081253df754a981
marcoa1818 <marcoachetos10@gmail.com>
2025-01-17 15:20:12 -0600

    Practica-04
    Se implementaron endpoints con los mecanimos necesarios para mostrar los mensajes/datos necesarios

---
+++ app.js
@@ -0,0 +1,100 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import {v4 as uuidv4} from 'uuid'
+import session from 'express-session'
+import moment from 'moment-timezone'
+import os from 'os'
+
+const app = express();
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.listen(3000,()=>{
+    console.log("Servidor corriendo en el puerto 3000")
+})
+// Configuración de las sesiones
+app.use(session({
+    secret:"p4-JDR#witchsoda-sesioneshttp",
+    resave:false,
+    saveUninitialized:false,
+    cookie:{maxAge:5*60*1000}
+}))
+// Sesiones almacenadas en Memoria (RAM)
+const sessions = {}
+//Funcion que permite acceder a la información de la interfaz de red en este caso LAN
+const getClientIp = (req) =>{
+    return(
+        req.header["x-forwarded-for"] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket?.remoteAddress
+    )
+}
+// Login endpoint
+app.post("/login",(req,res)=>{
+    console.log(req.body)
+    const {email, nickname, macAddress}=req.body;
+    if (!email || !nickname || !macAddress){
+        return res.status(400).json({message:"Missing required fields"});
+    }
+    const sessionId = uuidv4();
+    const now = new Date();
+    sessions[sessionId]={
+        sessionId,
+        email,
+        nickname,
+        macAddress,
+        ip: getClientIp(req),
+        createdAt:now,
+        lastAccess:now
+    }
+
+    res.status(200).json({
+        message:"Se ha logeado de manera exitosa",
+        sessionId,
+    })
+
+    //Logout endpoint
+    app.post("/logout",(req,res)=>{
+        const {sessionId} = req.body;
+        if (!sessionId || !sessions[sessionId]){
+            return res.status(404).json({
+                message:"No se ha encontrado una sesión activa."
+            })
+        }
+        delete sessions[sessionId]
+        req.session.destroy((err)=>{
+            if (err){
+                return res.status(500).send('Error al cerrar la sesión');
+            }
+        })
+        res.status(200).json({message:"Logout succesful"})
+    })
+
+    //Actualización de la Sesión
+    app.put("/update",(req,res)=>{
+        const {sessionId,email,nickname} = req.body;
+        if (!sessionId || !sessions[sessionId]){
+            return res.status(404).json({message:"No existe una sesión activa"});
+        }
+        if (email) sessions[sessionId].email = email
+        if(nickname) sessions[sessionId].nickname = nickname;
+        sessions[sessionId].lastAccess = new Date()
+        res.status(200).json({
+            message:"La sesión ha sido actualizada",
+            session: session[sessionId]
+        })
+    })
+
+    //Estatus
+    app.get("/status",(req,res)=>{
+        const sessionId = req.query.sessionId;
+
+        if(!sessionId || !sessions[sessionId]){
+            res.status(404).json({message:"No hay sesión activa"})
+        }
+        res.status(200).json({
+            message:"Sesion Activa",
+            session:sessions[sessionId]
+        })
+    })
+})
